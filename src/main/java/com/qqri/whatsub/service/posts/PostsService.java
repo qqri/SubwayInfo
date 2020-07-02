88f477be4eab05454353dde966e9ec74bca0f280
qqri <50269335+qqri@users.noreply.github.com>
2020-07-02 16:47:43 +0900

    feat : 글 삭제 추가

--- src/main/java/com/qqri/whatsub/service/posts/PostsService.java
+++ src/main/java/com/qqri/whatsub/service/posts/PostsService.java
@@ -58,4 +58,12 @@ public class PostsService {
                 .map(PostsListResponseDto :: new)
                 .collect(Collectors.toList());
     }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id) //JPA에서 지원중인 메소드
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
+    }
 }
--- src/main/java/com/qqri/whatsub/web/PostsApiController.java
+++ src/main/java/com/qqri/whatsub/web/PostsApiController.java
@@ -34,6 +34,11 @@ public class PostsApiController {
         return postsService.findById(id);
     }
 
-
+    //삭제
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 
 }
