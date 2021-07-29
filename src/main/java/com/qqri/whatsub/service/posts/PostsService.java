c0e08cf657cef203bef40e8dff207cc16e2c016c
qqri <50269335+qqri@users.noreply.github.com>
2021-07-29 14:06:15 +0900

    Update PostsService.java

--- src/main/java/com/qqri/whatsub/service/posts/PostsService.java
+++ src/main/java/com/qqri/whatsub/service/posts/PostsService.java
@@ -7,17 +7,108 @@ import com.qqri.whatsub.web.dto.PostsResponseDto;
 import com.qqri.whatsub.web.dto.PostsSaveRequestDto;
 import com.qqri.whatsub.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
-
 @RequiredArgsConstructor
 @Service
 public class PostsService {
 
     private final PostsRepository postsRepository;
+    private WebDriver driver;
+    public static final String WEB_DRIVER_ID = "webdriver.chrome.driver";
+    public static final String WEB_DRIVER_PATH = "src/main/resources/chromedriver.exe";
+
+    @PostConstruct
+    public void test() {
+       // Path path = Paths.get(System.getProperty("user.dir"), "src/main/resources/chromedriver.exe");  // 현재 package의
+        Path path = Paths.get(System.getProperty("user.dir"), "src/main/resources/chromedriver.exe");  // 현재 package의
+
+        // WebDriver 경로 설정
+        System.setProperty("webdriver.chrome.driver", path.toString());
+
+        // WebDriver 옵션 설정
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");            // 전체화면으로 실행
+        options.addArguments("--disable-popup-blocking");    // 팝업 무시
+        options.addArguments("--disable-default-apps");     // 기본앱 사용안함
+        //options.addArguments("--headless");
+        // WebDriver 객체 생성
+        ChromeDriver driver = new ChromeDriver( options );
+
+        // 빈 탭 생성
+
+        driver.get("https://finance.yahoo.com/quote/AAPL/history?p=AAPL");
+        //driver.get("https://heodolf.tistory.com/101");
+        for(int i = 0 ; i < 3 ; i++ ) {
+            ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, 2000)");
+            // 첫번째 탭으로 전환
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+//        WebElement page1_title = driver.findElementByXPath("//*[@id=\"content\"]/div[1]/div[1]/div/h1");
+//        if( page1_title != null  ) {
+//            System.out.println( page1_title.getText() );
+//        }
+        driver.close();
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // WebDriver 종료
+            driver.quit();
+        }
+    }
+
+
+
+    public void getKoreaCovidDatas() throws IOException {
+        String STOCK_DATAS_URL = "https://finance.yahoo.com/quote/GOOG/history";
+        Document doc = Jsoup.connect(STOCK_DATAS_URL).get();
+        Elements contents = doc.select("div table tbody tr");
+        //1. 이중 포문으로 검사하는 경우
+        Elements sellContent, buyContent ;
+        int count = 20;
+        float temp =0 , tempMin,tempLow, maxProfit = 0;
+        String tempLowDate = "" , MinDate ="", MaxDate="";
+
+        for(int i =0 ; i < count-1 ; i++) {
+            sellContent = contents.get(i).select("td");
+            temp = Float.parseFloat(sellContent.get(2).text().replace(",", ""));
+            tempMin = 98765432;
+
+            for (int j = i + 1; j < count; j++) {
+                buyContent = contents.get(j).select("td");
+                tempLow = Float.parseFloat(buyContent.get(3).text().replace(",", ""));
+                if (tempLow > temp) continue;
+                if (tempMin > tempLow) {
+                    tempMin = tempLow;
+                    tempLowDate = buyContent.get(0).text();
+                }
+            }
+        }
+
+        System.out.println("buy date : " + tempLowDate);
+    }
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
