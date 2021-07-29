2d9a0d5738bb0ba1fda9b95132de6a36f9b1a661
qqri <50269335+qqri@users.noreply.github.com>
2021-07-29 14:06:08 +0900

    Update IndexController.java

--- src/main/java/com/qqri/whatsub/web/IndexController.java
+++ src/main/java/com/qqri/whatsub/web/IndexController.java
@@ -18,12 +18,12 @@ public class IndexController {
 * model : 서버 템플릿 엔진에서 사용할 수 있는 개체를 저장할 수 있다.
 * 여기서는 postsService.findAllDesc() 로 가져온 결과를 posts 로 index.mustache 에 전달한다.
 * */
-    @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("posts", postsService.findAllDesc());
-
-        return "index";
-    }
+//    @GetMapping("/")
+//    public String index(Model model){
+//        model.addAttribute("posts", postsService.findAllDesc());
+//
+//        return "index";
+//    }
 
     @GetMapping("/subway")
     public String toSubway() {
